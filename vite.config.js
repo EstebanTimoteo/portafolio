d406163afcab06a1d58916f39d83080d45527ef5
Esteban Timoteo <nabirp_nabetse@hotmail.com>
2026-05-08 14:26:30 -0600

    borrado de una linea en viite config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import { resolve } from "path";
 
 export default defineConfig({
   // SUSTITUYE 'galeria_arte_moderno' por el nombre exacto de tu repositorio
-  base: "/galeria_arte_moderno/",
   base: './',
 
   build: {
